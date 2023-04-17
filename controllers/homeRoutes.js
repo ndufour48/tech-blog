650a6fcb5c496d78ef6bfe5223ec2fc8e0ed0598
Nicholas DuFour <nick.dufour48@gmail.com>
2023-04-17 03:23:42 -0400

    work on home routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const { Post, User } = require("../models");
+
+// get all
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+    const posts = postData.map((post) => post.get({ plain: true}))
+    res.render("homepage", {
+        posts,
+        logged_in: req.session.logged_in
+    })
+  } catch (err){
+      res.status(500).json(err)
+  }
+});
